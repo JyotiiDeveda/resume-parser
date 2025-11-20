22553da9c01a2e39d1b67960a1c696147c80c22f
Jyoti Deveda <jyoti@gkmit.co>
2025-11-20 15:14:44 +0530

    add readme

--- constants/mimetypes.constant.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  GOOGLE_DOC_MIME: 'application/vnd.google-apps.document',
-  DOCX_MIME: 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
-  PDF_MIME: 'application/pdf',
-  // Export Formats
-  EXPORT_GOOGLE_DOC_TO_DOCX: 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
-};
--- migrations/20251118115257-create-vector-extension.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  up: queryInterface => {
-    return queryInterface.sequelize.query('CREATE EXTENSION IF NOT EXISTS "vector";');
-  },
-  down: queryInterface => {
-    return queryInterface.sequelize.query('DROP EXTENSION "vector";');
-  }
-};
